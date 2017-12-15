7375acf7dfd50dabb6d4f9a6e782b8b7c9346347
Megan <megan.e.dose@gmail.com>
2017-12-14 17:33:34 -0800

    Added some info to the About page

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ class About extends Component {
   render () {
     return (
       <div>
-      Testing the About route
+        <img src='/PersonalPhoto.jpg' alt='Profile image of Megan' />
+        <p>Megan graduated from Louisiana State University with a Bachelor of Arts in Art History. She has experience with multiple museum industry roles, serving as a registrar and curator intern with LSU's Museum of Art and then as a tour guide and customer service associate at the Walt Disney Family Museum. These experiences have given her a wide set of practical skills that can transfer to the tech world, including strong organizational skills, the creativity to parse a large story to the specific needs of the different audiences, and problem solving on a day-to-day basis. She is looking forward to combining these skills with her newfound love of coding.</p>
       </div>
     )
   }
